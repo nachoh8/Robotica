9c47c4444824ae4ea2e2144756496d906745cc39
nacho <nachohseara@gmail.com>
2021-04-08 14:07:53 +0000

    entrega P4 bien detalles

--- p4_base.py
+++ p4_base.py
@@ -18,9 +18,9 @@ def main(args):
             print('Map file %s does not exist' % args.mapfile)
             exit(1)
 
-        map_file = args.mapfile;
-        init_pos = (args.initx, args.inity);
-        final_pos = (args.finalx, args.finaly);
+        map_file = args.mapfile
+        init_pos = (args.initx, args.inity
+        final_pos = (args.finalx, args.finaly)
         # Instantiate Odometry with your own files from P2/P3
         # inicializar el robot en la mitad de la baldosa (0.4x0.4)
         init_x = init_pos[0]* 0.4 + 0.2
@@ -31,14 +31,15 @@ def main(args):
         # 1. load map and compute costs and path
         myMap = Map2D(map_file)
         path = myMap.planPath(init_pos[0], init_pos[1], final_pos[0], final_pos[1])
-        if path is None: exit(1)
+        if path is None:
+			print("No he encontrado el camino")
+			exit(1)
         print(path)
        
         # 2. launch updateOdometry thread()
         robot.startOdometry()
 
         # 3. perform trajectory
-        # robot.setSpeed(1,1) ...
         while len(path) > 0:
             next_pos = path.pop(0)
             dir_pos = ((next_pos[0] - init_pos[0]) * 0.4, (next_pos[1] - init_pos[1]) * 0.4)
@@ -52,15 +53,15 @@ def main(args):
                 
 				# Volver a planear la ruta
                 path = myMap.planPath(init_pos[0], init_pos[1], final_pos[0], final_pos[1])
-                if path is None: break
+                if path is None:
+					print("Me he perdido")
+					break
                 print(path)
             else:
                 init_pos = next_pos
-				
-            # check if there are close obstacles
-            # deal with them...
-            # Avoid_obstacle(...) OR RePlanPath(...)
-        
+                
+        print("Ha llegado a su destino")
+				        
         # 4. wrap up and close stuff ...
         # This currently unconfigure the sensors, disable the motors,
         # and restore the LED to the control of the BrickPi3 firmware.
